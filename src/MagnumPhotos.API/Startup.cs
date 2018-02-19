e9e8469cc68fa7286a80478bd77ade08b0648888
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-19 10:23:21 -0500

    Wire up services

--- src/MagnumPhotos.API/Startup.cs
+++ src/MagnumPhotos.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -11,9 +12,10 @@ using Microsoft.Extensions.Options;
 using MagnumPhotos.API.Services.Interfaces;
 using MagnumPhotos.API.Services;
 using MagnumPhotos.API.Entities;
+using MagnumPhotos.API.Helpers;
 using MagnumPhotos.API.Data.Seed;
 using MagnumPhotos.API.Data.Context;
-using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
 
 namespace MagnumPhotos.API
 {
@@ -29,13 +31,29 @@ namespace MagnumPhotos.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            .AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+            });
 
             services.AddDbContext<MagnumPhotosContext> (options =>
                 options.UseNpgsql (Configuration.GetConnectionString ("MagnumPhotosApi")));
 
-            services.AddTransient<ITypeHelperService, TypeHelperService>();
-
             services.AddScoped<IMagnumPhotosRepository, MagnumPhotosRepository>();
+
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            services.AddScoped<IUrlHelper>(implementationFactory =>
+            {
+                var actionContext = implementationFactory.GetService<IActionContextAccessor>()
+                .ActionContext;
+                return new UrlHelper(actionContext);
+            });
+
+            services.AddTransient<IPropertyMappingService, PropertyMappingService>();
+
+            services.AddScoped<IUrlHelper, UrlHelper>(); */
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,
@@ -45,6 +63,24 @@ namespace MagnumPhotos.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Entities.Photographer, Models.PhotographerDto>()
+                    .ForMember(dest => dest.Name, opt => opt.MapFrom(src =>
+                    $"{src.FirstName} {src.LastName}"))
+                    .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
+                    src.DateOfBirth.GetCurrentAge()));
+
+                cfg.CreateMap<Entities.Book, Models.BookDto>();
+
+                cfg.CreateMap<Models.PhotographerForCreationDto, Entities.Photographer>();
+
+                cfg.CreateMap<Models.BookForCreationDto, Entities.Book>();
+
+                cfg.CreateMap<Entities.Book, Models.BookForUpdateDto>();
+            });
 
             app.UseMvc();
         }

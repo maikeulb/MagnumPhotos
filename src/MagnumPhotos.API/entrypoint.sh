87e2dd7d0edfbd0ef2aa5e5b2060ce165d39691f
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-08 07:34:00 -0500

    update Dockerfile to take advantage of multi-stage build

--- src/MagnumPhotos.API/entrypoint.sh
+++ src/MagnumPhotos.API/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 run_cmd="dotnet run --server.urls http://*:80"
 
 until dotnet ef database update; do
->&2 echo "SQL Server is starting up"
+>&2 echo "DB Server is starting up"
 sleep 1
 done
 
->&2 echo "SQL Server is up - executing command"
+>&2 echo "DB Server is up - executing command"
 exec $run_cmd
